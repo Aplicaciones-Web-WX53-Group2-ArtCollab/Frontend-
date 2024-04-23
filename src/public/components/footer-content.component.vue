579ccf3189b667a4a835ecbde4238ec2b4c5e6f9
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-23 12:56:13 -0500

    feat: made footer-content responsive

--- src/public/components/footer-content.component.vue
+++ src/public/components/footer-content.component.vue
@@ -6,19 +6,19 @@ export default {
 </script>
 
 <template>
-  <footer>
-    <div  aria-label="The footer content" class="bg-cyan-600 p-4 text-white flex justify-content-center align-items-center flex-column gap-3">
-      <div class="footer-logo flex flex-row align-items-center justify-content-center gap-3">
+  <footer class="bg-cyan-600 p-4 text-white">
+    <div  aria-label="The footer content" class="flex flex-row justify-content-center align-items-center md:flex-column md:gap-3 gap-5">
+      <div class="footer-logo flex md:flex-row flex-column md:align-items-center align-items-start justify-content-center gap-3 ">
         <img src="../../assets/logo.png" alt="ArtCollab Logo" width="50" />
         <h2> ArtCollab </h2>
       </div>
-       <div class="flex gap-3 ">
+       <div class="flex gap-3 flex md:flex-row flex-column md:align-items-center align-items-start">
          <pv-inputicon class="pi pi-facebook text-2xl"   />
           <pv-inputicon class="pi pi-twitter text-2xl" />
           <pv-inputicon class="pi pi-instagram text-2xl" />
           <pv-inputicon class="pi pi-youtube text-2xl" />
        </div>
-        <div class="flex gap-5 footer-links">
+        <div class="flex md:gap-5 gap-3 footer-links flex md:flex-row flex-column md:align-items-center align-items-start">
           <p> {{ $t('about') }} </p>
           <p> {{ $t('gender') }} </p>
           <p> {{ $t('popular') }} </p>
@@ -26,9 +26,9 @@ export default {
           <p> {{ $t('monetization') }} </p>
           <p> {{ $t('artists') }} </p>
         </div>
-       <div>
-          <p> Copyright &copy; 2024 ArtCollab. {{ $t('copyright') }} </p>
-       </div>
+    </div>
+    <div class="copyright mt-3" aria-label="Copyright Information">
+      <p> Copyright &copy; 2024 ArtCollab. {{ $t('copyright') }} </p>
     </div>
   </footer>
 </template>
@@ -45,5 +45,8 @@ export default {
 }
 .footer-logo{
   cursor:pointer;
+}
+.copyright p{
+  text-align:center;
 }
 </style>
