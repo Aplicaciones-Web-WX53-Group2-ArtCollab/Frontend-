9816753fe7756caecca6545b47d386bb7fb4f7df
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-23 12:18:21 -0500

    chore(es.js): added spanish configs

--- src/locale/es.js
+++ src/locale/es.js
@@ -1,3 +1,12 @@
-const es = {}
+const es = {
+  gender: 'Género',
+  popular: 'Populares',
+  subscription: 'Suscripciones',
+  monetization: 'Monetización',
+  artists: 'Artistas',
+  login: 'Iniciar sesión',
+  about: 'Acerca de',
+  copyright: 'Todos los derechos reservados',
+}
 
 export default es
