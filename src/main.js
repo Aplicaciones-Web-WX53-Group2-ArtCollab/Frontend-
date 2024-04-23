0a0a6bc37256ecb2720a0c58b009cb006271b21f
CamiAm404 <u202114248@upc.edu.pe>
2024-04-23 11:23:30 -0500

    chore(main.js): added i18n configs

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import Toolbar from 'primevue/toolbar';
 import IconField from 'primevue/iconfield';
 import InputIcon from 'primevue/inputicon';
 import InputText from 'primevue/inputtext';
+import i18n from '@/locale/i18n.js'
 
 
 import 'primevue/resources/themes/aura-light-green/theme.css'
@@ -24,6 +25,6 @@ app.component('pv-toolbar', Toolbar);
 app.component('pv-iconfield', IconField);
 app.component('pv-inputicon', InputIcon);
 app.component('pv-inputtext', InputText);
-
+app.use(i18n);
 
 app.mount('#app');
