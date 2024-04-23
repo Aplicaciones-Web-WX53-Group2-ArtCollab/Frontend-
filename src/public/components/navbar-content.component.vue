5266eeaa655be6ae941ed55f4a67dd1be28314e0
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-22 22:52:23 -0500

    fix(footer-content/navbar-content): naming directories fixed

---
+++ src/public/components/navbar-content.component.vue
@@ -0,0 +1,32 @@
+<script>
+
+
+export default {
+  name: 'navbar-content',
+}
+</script>
+
+<template>
+  <pv-toolbar>
+    <template #start>
+      <Button icon="pi pi-plus" class="mr-2" severity="secondary" />
+      <Button icon="pi pi-print" class="mr-2" severity="secondary" />
+      <Button icon="pi pi-upload" severity="secondary" />
+    </template>
+
+    <template #center>
+      <IconField iconPosition="left">
+        <InputIcon>
+          <i class="pi pi-search" />
+        </InputIcon>
+        <InputText placeholder="Search" />
+      </IconField>
+    </template>
+
+    <template #end> <SplitButton label="Save" :model="items"></SplitButton></template>
+  </pv-toolbar>
+</template>
+
+<style scoped>
+
+</style>
