a2b771425112d76605c225dcd557592d9bc56d0c
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-22 22:48:02 -0500

    chore(main.js): import button and toolbar components for primevue

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue';
 
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
+import  Button  from 'primevue/button';
+import Toolbar from 'primevue/toolbar';
+
 
 
 const app = createApp(App);
 app.use(PrimeVue);
+app.component('pv-button', Button);
+app.component('pv-toolbar', Toolbar);
 
 
 app.mount('#app');
