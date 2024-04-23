260dfd9dbb960470804d2ec9da3db170c2e3b08e
Frida-Dam <u202218352@upc.edu.pe>
2024-04-22 23:44:35 -0500

    feat: Added Spanish translation

--- src/locale/es.js
+++ src/locale/es.js
@@ -1,3 +1,13 @@
-const es = {}
+const es = {
+  genre:'Géneros',
+  popular:'Populares',
+  subscription:'Suscripción',
+  monetization:'Monetización',
+  artist:'Artistas',
+  about: 'Acerca de',
+  copyright:'Copyright @2024 ArtCollab. Todos los derechos resevados.',
+
+}
+
 
 export default es
