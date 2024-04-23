c7aaf2915c34d956ce5a28d4142982a03d22b4e1
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-23 00:06:57 -0500

    feat(navbar)/chore(main.js): added navbar content and iconfield and inputicon components

--- src/public/components/navbar-content.component.vue
+++ src/public/components/navbar-content.component.vue
@@ -20,16 +20,21 @@ export default {
 
 <template>
   <div class="card">
-    <pv-toolbar class="flex justify-center items-center">
+    <pv-toolbar class="flex justify-content-center items-center">
       <template #start>
-        <div class="flex flex-row text-xs gap-3 ">
-          <img src="../../assets/logo.png" width="50" />
-          <h1> Géneros </h1>
-          <h1> Populares </h1>
-          <h1> Suscripciones </h1>
-          <h1> Monetización </h1>
-          <h1> Artistas </h1>
-        </div>
+       <div class=" flex gap-7">
+         <div class="flex flex-row text-xs gap-5 ">
+           <img src="../../assets/logo.png" width="40" />
+           <h2> Géneros </h2>
+           <h2> Populares </h2>
+           <h2> Suscripciones </h2>
+           <h2> Monetización </h2>
+           <h2> Artistas </h2>
+         </div>
+         <div>
+           <pv-button class=" bg-cyan-600" label="Iniciar Sesion"/>
+         </div>
+       </div>
       </template>
     </pv-toolbar>
   </div>
