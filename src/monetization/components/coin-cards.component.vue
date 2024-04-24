e83cf0a92f60bfa35ee321d234a3b2175e1e01ee
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-24 17:17:40 -0500

    feat(monetization): added monetization page content

---
+++ src/monetization/components/coin-cards.component.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'the-con-cards'
+}
+</script>
+
+<template>
+   <div>
+       <pv-card>
+         <template #title>Simple Card</template>
+         <template #content>
+           <p class="m-0">
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+             quas!
+           </p>
+         </template>
+       </pv-card>
+   </div>
+</template>
+
+<style scoped>
+
+</style>
