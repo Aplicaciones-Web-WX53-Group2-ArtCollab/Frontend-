155504882f2003b1c4aabc7dc654674f72effe70
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-24 18:04:09 -0500

    chore: updated branch dependencies to complete merge

--- src/content/pages/main-page.vue
+++ src/content/pages/main-page.vue
@@ -1,11 +1,15 @@
 <script setup>
 import MainPageBanner from '@/content/components/main-page/the-main-page-banner.component.vue'
 import MainPageGenres from '@/content/components/main-page/the-main-page-genres.component.vue'
+import NavbarContent from '@/public/components/navbar-content.component.vue'
+import FooterContent from '@/public/components/footer-content.component.vue'
 </script>
 
 <template>
+  <navbar-content/>
   <main-page-banner/>
   <main-page-genres/>
+  <footer-content/>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -5,18 +5,41 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import MainPage from '@/content/pages/main-page.vue';
 
-import Toolbar from 'primevue/toolbar';
-import Button from 'primevue/button';
+
 import 'primevue/resources/themes/aura-light-green/theme.css';
 import PrimeVue from 'primevue/config'
 import Carousel from 'primevue/carousel'
 import Galleria from 'primevue/galleria'
+import Toolbar from 'primevue/toolbar';
+import Button from 'primevue/button';
+import InputIcon from 'primevue/inputicon'
+import Sidebar from 'primevue/sidebar'
+import IconField from 'primevue/iconfield'
+import InputText from 'primevue/inputtext'
 
-const app = createApp(App);
+import i18n from '@/locale/i18n.js'
+import VueMq from 'vue3-mq'
+
+const app = createApp(MainPage);
 
 app.use(PrimeVue)
 app.component('pv-carousel', Carousel)
 app.component('pv-button', Button)
 app.component('pv-toolbar', Toolbar)
 app.component('pv-galleria', Galleria)
+app.component('pv-inputicon', InputIcon)
+app.component('pv-sidebar', Sidebar)
+app.component('pv-iconfield', IconField)
+app.component('pv-inputtext', InputText);
+app.use(i18n);
+
+app.use(VueMq, {
+  breakpoints: {
+    sm: 640,
+    md: 768,
+    lg: Infinity
+  },
+  defaultBreakpoint: 'sm',
+});
+
 app.mount('#app')
