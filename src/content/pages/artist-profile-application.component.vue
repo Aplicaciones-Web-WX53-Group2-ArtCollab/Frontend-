69a1fd8e7b549633274182f2f7c591cbfb10e9fd
CamiAm404 <u202114248@upc.edu.pe>
2024-04-24 18:07:27 -0500

    feat: Added artist profile component and page

---
+++ src/content/pages/artist-profile-application.component.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ArtistProfile from '@/content/components/profiles/the-artist-profile.component.vue'
+</script>
+
+<template>
+<artist-profile/>
+</template>
+
+<style scoped>
+
+</style>
