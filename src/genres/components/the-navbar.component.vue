9e3fe548382792acd0827f52e7bbf531deef4a83
Frida-Dam <u202218352@upc.edu.pe>
2024-04-23 21:54:02 -0500

    feat: added the-navbar.component

---
+++ src/genres/components/the-navbar.component.vue
@@ -0,0 +1,25 @@
+<script >
+import PvToolbar from '@/main.js'
+import PvButton from '@/main.js'
+
+export default {
+  name:'genres-navbar',
+  components: { PvButton, PvToolbar }
+}
+</script>
+
+<template>
+  <pv-toolbar class="flex justify-content-center items-center w-full" aria-label="Toolbar content">
+    <template #start>
+      <div class="flex align-content-center gap-4">
+        <pv-button label="romance-button" Romance aria-label="Romance"/>
+        <pv-button label="fantasy-button" Fantasia aria-label="Fantasy"/>
+        <pv-button label="comedy-button" Comedia aria-label="Comedy"/>
+      </div>
+    </template>
+    <pv-toolbar/>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import Toolbar from 'primevue/toolbar';
+import Button from 'primevue/button';
+import 'primevue/resources/themes/aura-light-green/theme.css';
 import PrimeVue from 'primevue/config'
 
-const app = createApp(App)
+const app = createApp(App);
+
+app.component('pv-toolbar', Toolbar);
+app.component('pv-button', Button);
+
 app.use(PrimeVue)
 app.mount('#app')
