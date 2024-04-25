21067799106790a77ba5db793f01e870ec60be4d
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-04-24 20:31:36 -0500

    fix(navbar): fixed responsive layouts

--- src/public/components/navbar-content.component.vue
+++ src/public/components/navbar-content.component.vue
@@ -20,58 +20,59 @@ export default {
 </script>
 
 <template>
-    <pv-toolbar class="flex justify-content-center items-center toolbar" aria-label="Toolbar content">
-      <template #start>
-        <div class=" flex flex-row gap-7 justify-content-center" aria-label="Main navigation section">
-          <img src="../../assets/logo.png" width="40" height="40" alt="ArtCollab Logo"/>
-          <div class="flex justify-content-center">
-            <div  class=" block lg:hidden card flex justify-content-center" aria-label="toolbar features content">
-              <pv-sidebar   v-model:visible="visible" header="Artcollab">
-                <img src="../../assets/logo.png" width="40" height="40" alt="ArtCollab Logo"/>
-                <h2> {{ $t('gender') }} </h2>
-                <h2> {{ $t('popular')}} </h2>
-                <h2> {{ $t('subscription') }} </h2>
-                <h2> {{ $t('monetization') }} </h2>
-                <h2 > {{ $t('artists') }} </h2>
-              </pv-sidebar>
-              <pv-button class="bg-transparent text-black-alpha-80 border-transparent text-2xl hover:text-cyan-600"  icon="pi pi-bars"  @click="visible = true" aria-label="Menu" />
-            </div>
-            <div  class=" hidden  text-center lg:flex lg:flex-row text-xs gap-5 align-items-center navbar-links " aria-label="toolbar features content" >
+  <pv-toolbar class="flex justify-content-center items-center toolbar" aria-label="Toolbar content">
+    <template #start>
+      <div class=" flex flex-row gap-7 justify-content-center" aria-label="Main navigation section">
+        <img class="lg:block hidden" src="../../assets/logo.png" width="40" height="40" alt="ArtCollab Logo"/>
+        <div class="flex justify-content-center">
+          <div  class=" block lg:hidden card flex justify-content-center" aria-label="toolbar features content">
+            <pv-sidebar   v-model:visible="visible" header="Artcollab">
+              <img src="../../assets/logo.png" width="40" height="40" alt="ArtCollab Logo"/>
               <h2> {{ $t('gender') }} </h2>
               <h2> {{ $t('popular')}} </h2>
               <h2> {{ $t('subscription') }} </h2>
               <h2> {{ $t('monetization') }} </h2>
               <h2 > {{ $t('artists') }} </h2>
-            </div>
+              <pv-button class=" bg-cyan-600" label="Iniciar Sesion"> {{ $t('login') }} </pv-button>
+            </pv-sidebar>
+            <pv-button class="bg-transparent text-black-alpha-80 border-transparent text-2xl hover:text-cyan-600"  icon="pi pi-bars"  @click="visible = true" aria-label="Menu" />
           </div>
-          <div class="card flex flex-wrap justify-content-center gap-3" aria-label="search buttons content">
-            <pv-iconfield icon-position="left" >
-              <pv-inputicon class="pi pi-search text-black-alpha-90 text-2xl absolute mt-1 top-0"></pv-inputicon>
-              <pv-inputtext  class="pl-6" />
-            </pv-iconfield>
-          </div>
-          <div aria-label="login button">
-            <pv-button class=" bg-cyan-600" label="Iniciar Sesion"> {{ $t('login') }} </pv-button>
+          <div  class=" hidden  text-center lg:flex lg:flex-row text-xs gap-5 align-items-center navbar-links " aria-label="toolbar features content" >
+            <h2> {{ $t('gender') }} </h2>
+            <h2> {{ $t('popular')}} </h2>
+            <h2> {{ $t('subscription') }} </h2>
+            <h2> {{ $t('monetization') }} </h2>
+            <h2 > {{ $t('artists') }} </h2>
           </div>
         </div>
-      </template>
-    </pv-toolbar>
+        <div class="card flex flex-wrap justify-content-center gap-3" aria-label="search buttons content">
+          <pv-iconfield icon-position="left" >
+            <pv-inputicon class="pi pi-search text-black-alpha-90 text-2xl absolute mt-1 top-0"></pv-inputicon>
+            <pv-inputtext  class="pl-6" />
+          </pv-iconfield>
+        </div>
+        <div aria-label="login button">
+          <pv-button class="lg:block hidden bg-cyan-600" label="Iniciar Sesion"> {{ $t('login') }} </pv-button>
+        </div>
+      </div>
+    </template>
+  </pv-toolbar>
 </template>
 
 <style scoped>
 .toolbar{
   border-radius:0;
 }
- h2:hover{
-    cursor:pointer;
-    color: #00B4CC;
- }
-  h2{
-   color:black;
-   font-size: 1rem;
-   font-weight: bold;
- }
- .navbar-links img {
-   cursor: pointer;
- }
+h2:hover{
+  cursor:pointer;
+  color: #00B4CC;
+}
+h2{
+  color:black;
+  font-size: 1rem;
+  font-weight: bold;
+}
+.navbar-links img {
+  cursor: pointer;
+}
 </style>
