18550a3e38d35080002c8d813843d7bf2a11ba70
Frida-Dam <u202218352@upc.edu.pe>
2024-04-25 18:14:02 -0500

    feat: added book cards components for genres

---
+++ src/genres/components/fantasy-book-card.component.vue
@@ -0,0 +1,88 @@
+<script >
+import { BookApiFake } from '@/genres/services/book-api-fake.services.js'
+import { Book } from '@/genres/models/romance-book.entity.js'
+
+
+export default {
+  name: 'fantasy-books-cards',
+  data(){
+    return{
+      books: [],
+      bookApiFake:new BookApiFake()
+    }
+  },
+  async created() {
+    this.bookApiFake.getFantasyBooks().then((response) => {
+      for (const bookData of response.data){
+        const { name, image, author}= bookData;
+        const book = new Book(name, image, author);
+        this.books.push(book);
+      }
+    }).catch((error) => {
+      console.error('Error fetching animals:', error);
+    });
+
+  }
+
+}
+</script>
+
+
+<template>
+  <div>
+    <label>FantasyBooksCards</label>
+    <div class="fantasy-books-cards">
+      <div v-for="(book, index) in books" :key="index">
+        <pv-card class="card">
+          <template #header>
+            <img class="header" :src="book.getImage()" alt="Book cover" />
+          </template>
+          <template #title>
+            {{ book.getName() }}
+          </template>
+          <template #subtitle >
+            {{ book.getAuthor() }}
+          </template>
+          <template #footer>
+            <pv-rating class="rating" :cancel="false" :stars="1">
+              <template #onicon>
+                <i class="pi pi-heart-fill">13.8 M</i>
+              </template>
+              <template #officon>
+                <i class="pi pi-heart">13.7 M</i>
+              </template>
+            </pv-rating>
+
+          </template>
+        </pv-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.fantasy-books-cards {
+  display: grid;
+  grid-template-columns:  15rem 15rem 15rem ;
+  gap: 50px;
+  grid-auto-rows: auto;
+  margin: 200px;
+  margin-top: 100px;
+
+}
+
+.card {
+
+  justify-content: space-around;
+
+}
+
+.header {
+
+  background-color: #f1f1f1;
+  width: 15rem;
+  height: 20rem;
+  object-fit: cover;
+}
+</style>
