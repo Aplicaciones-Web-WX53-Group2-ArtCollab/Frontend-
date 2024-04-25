6c9ff36c02d88e7da14a5e4921475e1904d31cab
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-24 23:02:04 -0500

    feat: Completed main page

--- src/content/pages/main-page.vue
+++ src/content/pages/main-page.vue
@@ -4,16 +4,24 @@ import TheMainPageGenres from '@/content/components/main-page/the-main-page-genr
 import NavbarContent from '@/public/components/navbar-content.component.vue'
 import FooterContent from '@/public/components/footer-content.component.vue'
 import TheMainPagePopularity from '@/content/components/main-page/the-main-page-popularity.component.vue'
+import TheMainPageRecentBook from '@/content/components/main-page/the-main-page-recent-book.component.vue'
+import TheMainPagePopularArtist from '@/content/components/main-page/the-main-page-popular-artist.component.vue'
 </script>
 
 <template>
-  <navbar-content/>
-  <the-main-page-banner/>
-  <the-main-page-genres/>
-  <the-main-page-popularity/>
-  <footer-content/>
+  <section class="main-page">
+    <navbar-content/>
+    <the-main-page-banner/>
+    <the-main-page-genres/>
+    <the-main-page-popularity/>
+    <the-main-page-recent-book/>
+    <the-main-page-popular-artist/>
+    <footer-content/>
+  </section>
 </template>
 
 <style scoped>
 
 </style>
+
+<!--TODO: i18n for main page-->
