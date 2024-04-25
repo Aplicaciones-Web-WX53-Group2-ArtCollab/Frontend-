0ea7a8d745f3f1b47986f06bbf5d9406d19fd44a
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-25 11:03:33 -0500

    chore: installed project dependencies

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import './assets/main.css'
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import VueMq from 'vue3-mq'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/aura-light-green/theme.css';
+import PrimeVue from 'primevue/config'
+import ToastService from "primevue/toastservice";
+
+const app = createApp(App);
+
+app.use(PrimeVue)
+app.use(ToastService)
+
+app.use(VueMq, {
+  breakpoints: {
+    sm: 640,
+    md: 768,
+    lg: Infinity
+  },
+  defaultBreakpoint: 'sm',
+});
+
+app.mount('#app')
---
+++ src/shared/user-login.component.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<section class="user-login bg-cyan-50">
+
+</section>
+</template>
+
+<style scoped>
+
+</style>
