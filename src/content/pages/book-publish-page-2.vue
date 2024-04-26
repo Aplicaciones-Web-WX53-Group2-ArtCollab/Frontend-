557ac9d193634af11f1e5b8e8b36c3840ee1cbf9
CamiAm404 <u202114248@upc.edu.pe>
2024-04-26 00:17:08 -0500

    feat(book-publish-page-2): Added component and improved style
    Added component and improved style by using prime flex properties.

--- src/content/pages/book-publish-page-2.vue
+++ src/content/pages/book-publish-page-2.vue
@@ -4,20 +4,27 @@ import FooterContent from '@/public/components/footer-content.component.vue'
 import BookBanner from '@/content/components/publish/book-publish-banner.component.vue'
 import BookInput from '@/content/components/publish/book-publish-input.component.vue'
 import BookFileUpload from '@/content/components/publish/book-publish-fileupload.component.vue'
+import BookPublishEditor from '@/content/components/publish/the-book-publish-editor.component.vue'
+
 </script>
 
 <template>
   <navbar-content/>
   <book-banner/>
-  <div class="container">
-    <div class="input">
-      <book-input borderStyle="none" outlineStyle="none" style="width: 80%" placeholder="Capítulo sin titulo" placeholderStyle="text-align: center; width: 100%;" size="large" />
+  <div class="input flex align-items-center justify-content-center">
+    <div class="mt-2 w-10">
+      <book-input class="text-xl" borderStyle="none" outlineStyle="none" placeholder="Capítulo sin titulo" placeholderStyle="text-align: center; width: 100%;" size="large" />
     </div>
-    <div class="divider">
-      <pv-divider style="width: 80%"  class="custom-divider" />
+  </div>
+  <div class="divider flex align-items-center justify-content-center">
+    <div class="mt-2 w-10">
+      <pv-divider class="custom-divider" />
     </div>
-    <div class="image-fileupload flex flex-column gap-2">
-      <label for="titulo" class="text-center">Subir ilustraciones</label>
+  </div>
+  <book-publish-editor/>
+  <div class="flex align-items-center justify-content-center">
+    <div class="image-fileupload flex flex-column gap-2 m-5">
+      <label for="titulo" class="text-center font-bold text-xl">Subir ilustraciones</label>
       <book-file-upload/>
     </div>
   </div>
@@ -25,40 +32,10 @@ import BookFileUpload from '@/content/components/publish/book-publish-fileupload
 </template>
 
 <style scoped>
-.input {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 20px;
-}
-
-.container {
-  margin-bottom:6.7rem;
-}
-
-.divider {
-  margin-top: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 
 .custom-divider::before {
   border-top-width: 5px;
   border-top-color: #012840;
-}
-
-.image-fileupload {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 20px;
-}
-
-label {
-  align-items: center;
-  font-weight: bold;
-  font-size: 1.2rem;
 }
 
 </style>
