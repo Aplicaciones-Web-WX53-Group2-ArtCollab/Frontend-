46d863ef99461201111ec394ae7ca5db0b8b26d7
Frida-Dam <u202218352@upc.edu.pe>
2024-04-26 02:08:36 -0500

    feat: added profile component

---
+++ src/user/components/the-user-profile.component.vue
@@ -0,0 +1,194 @@
+<script>
+export default {
+  name: 'the-user-profile',
+  data() {
+    return {
+      social_media: 'Social Media',
+      illustrated_books: 'Illustrated Books',
+      portfolio: 'Portfolio'
+    };
+  }
+};
+
+</script>
+
+<template>
+  <div class="user-profile">
+    <div class="banner">
+      <div class="banner-background"></div>
+      <div class="profile-image">
+        <img src="/src/assets/images/profile-picture.png" alt="User profile picture">
+        <div class="text">Flor M. Salvador</div>
+      </div>
+    </div>
+    <div class="flexbox-container">
+      <div class="flex-item">
+        <div class="social-media">
+          <div class="media-title">{{ $t('social_media') }}</div>
+          <div class="flex-container-2">
+            <img class="small-image" src="/src/assets/images/x-logo.png" alt="X logo">
+            <a href="https://www.x.com/" target="_blank">https://www.x.com/</a>
+          </div>
+          <div class="flex-container-2">
+            <img class="small-image" src="/src/assets/images/instagram-logo.png" alt="Instagram logo">
+            <a href="https://www.instagram.com/" target="_blank">https://www.instagram.com/</a>
+          </div>
+          <div class="flex-container-2">
+            <img class="small-image" src="/src/assets/images/facebook-logo.png" alt="Facebook logo">
+            <a href="https://www.facebook.com/" target="_blank">https://www.facebook.com/</a>
+          </div>
+          <div class="flex-container-2">
+            <img class="small-image" src="/src/assets/images/tiktok-logo.png" alt="Tiktok logo">
+            <a href="https://www.tiktok.com/" target="_blank">https://www.tiktok.com/</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex-item">
+        <div class="bio">
+          <div class="bio-title">Bio</div>
+          <div>Lorem ipsum dolor sit amet consectetur adipiscing elit mauris ornare malesuada himenaeos eleifend morbi, vivamus nostra faucibus platea nisl nec lacinia hendrerit ultricies id maecenas diam. Eget vel aliquet nam penatibus vulputate felis dapibus magnis montes auctor ut, augue placerat praesent class condimentum litora eros sociis iaculis.</div>
+        </div>
+        <div class="published-book">
+          <div class="published-book-title">{{ $t('published_books') }}</div>
+          <div class="flex-container">
+            <img class="normal-image" src=" " alt="Book image">
+            <img class="normal-image" src=" " alt="Book image">
+            <img class="normal-image" src=" " alt="Book image">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  overflow-x: hidden;
+}
+
+.banner {
+  position: relative;
+}
+
+.banner-background {
+  position: absolute;
+  top: 5%;
+  left: 0;
+  width: 100%;
+  height: 40vh; /* Altura del 75% del viewport height */
+  background-color: #f8cd7c;
+  z-index: 1; /* z-index menor para que esté detrás del otro fondo */
+}
+
+.profile-image {
+  position: relative; /* Asegúrate de que .elemento tenga una posición que no sea 'static' para que z-index funcione */
+  text-align:center;
+  padding: 20px;
+  z-index: 3; /* z-index aún mayor para que esté delante de ambos fondos */
+}
+
+.profile-image img {
+  position: relative;
+  z-index: 1; /* z-index menor para que la imagen esté detrás del texto */
+}
+
+.text {
+  position: relative;
+  color: black;
+  font: bold 30px/30px Archivo, sans-serif;
+  z-index: 2; /* z-index mayor para que el texto esté delante de la imagen */
+  top:-10%;
+}
+
+.flexbox-container {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-around;
+  width: 100%;
+}
+
+/* Estilos para los elementos flex dentro del contenedor */
+.flex-item {
+  padding: 10px;
+  max-width: 60vw;
+  font: 20px/30px Archivo, sans-serif;
+  color: black;
+}
+
+.flex-container-2 {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  max-width: 20vw;
+  margin: 15px 0 15px 0;
+}
+
+.flex-container img {
+  margin-right: 10px; /* Espacio entre las cajas */
+  margin-bottom: 10px; /* Espacio entre las cajas */
+  margin-top: 10px; /* Espacio entre las cajas */
+}
+
+.small-image {
+  width: 10%; /* Ajusta el ancho según tus necesidades */
+  height: auto; /* Mantiene la proporción de la imagen */
+  margin: 10px 10px 10px 0;
+}
+
+.normal-image {
+  width: 25%; /* Ajusta el ancho según tus necesidades */
+  height: auto; /* Mantiene la proporción de la imagen */
+  margin: 10px 10px 10px 0;
+}
+
+.large-image {
+  width: 30%; /* Ajusta el ancho según tus necesidades */
+  height: auto; /* Mantiene la proporción de la imagen */
+}
+
+.bio {
+  margin-top: 20px;
+}
+
+.bio-title {
+  font-weight: 600; /* Semibold */
+  margin-bottom: 10px;
+}
+
+.published-book {
+  margin-top: 20px;
+}
+
+.published-book-title {
+  font-weight: 600; /* Semibold */
+}
+
+.media-title {
+  font-weight: 600; /* Semibold */
+}
+
+.user-books {
+  margin-top: 20px;
+}
+
+.use-books-title {
+  font-weight: 600; /* Semibold */
+}
+
+.flex-container p {
+  display: flex;
+  justify-content: center;
+}
+
+.flex-container-2 a{
+  display: flex;
+  justify-content: center;
+  color: black;
+  text-decoration: underline;
+}
+</style>
