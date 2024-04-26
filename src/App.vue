bc8266ec300103c82cacd47c2ed2a577e6799f78
CamiAm404 <u202114248@upc.edu.pe>
2024-04-26 12:58:59 -0500

    feat(App.vue/book-publish-page-2): Added buttons to page and imported it into app
    Added buttons to the book publish second page and imported it into app.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 
 import ArtistProfilePage from '.././src/content/pages/artist-profile-page.vue'
+import BookPublishPage2 from '@/content/pages/book-publish-page-2.vue'
 </script>
 
 <template>
-  <artist-profile-page/>
+<!--  <artist-profile-page/>-->
+  <book-publish-page2/>
 </template>
 
 <style scoped>
