3017f8e476e7f582137265b43a19bf18cb136d74
CamiAm404 <u202114248@upc.edu.pe>
2024-04-27 00:28:13 -0500

    feat(details-edit-tabview): Added info to tabpanel
    Added table of contents to second tabpanel

--- src/content/components/details-edit/the-book-details-edit-tabview.component.vue
+++ src/content/components/details-edit/the-book-details-edit-tabview.component.vue
@@ -25,6 +25,104 @@ const defaultValue2 = ref('¿Quién dijo que después de la tormenta sale el sol
         </pv-tabpanel>
       </div>
       <pv-tabpanel header="Tabla de contenidos">
+        <pv-card class="shadow-none">
+              <template #content>
+                <div class="table-of-contents flex flex-row gap-8">
+                  <div class="chapters flex flex-column pr-8 mr-6">
+                    <p class="pb-2">Prólogo</p>
+                    <p class="pb-2">Capítulo 1</p>
+                    <p class="pb-2">Capítulo 2</p>
+                    <p class="pb-2">Capítulo 3</p>
+                    <p class="pb-2">Capítulo 4</p>
+                    <p class="pb-2">Capítulo 5</p>
+                    <p class="pb-2">Capítulo 6</p>
+                    <p class="pb-2">Capítulo 7</p>
+                    <p class="pb-2">Capítulo 8</p>
+                    <p class="pb-2">Capítulo 9</p>
+                    <p class="pb-2">Capítulo 10</p>
+                  </div>
+                  <div class="flex flex-column pl-8 ml-6">
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>100 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>130 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>150 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>125 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>143 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>167 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p class="mr-2">80 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil "/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>101 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p class="mr-2">99 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>175 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                    <div class="right-side flex flex-row gap-2 pb-2">
+                      <i class="pi pi-heart-fill"/>
+                      <p>111 K</p>
+                      <div class="pencil-icon custom-icon bg-cyan-500 text-white-alpha-90 border-circle">
+                        <i class="pi pi-pencil"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </template>
+        </pv-card>
       </pv-tabpanel>
       <pv-tabpanel header="Ingresos obtenidos">
       </pv-tabpanel>
@@ -33,6 +131,10 @@ const defaultValue2 = ref('¿Quién dijo que después de la tormenta sale el sol
 </template>
 
 <style scoped>
-
-
+.custom-icon {
+  width: 1.5rem;
+  align-items:center;
+  justify-content:center;
+  display:flex;
+}
 </style>
