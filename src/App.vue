b717ac612c097759bad73d0836ce4bf3f2c41902
CamiAm404 <u202114248@upc.edu.pe>
2024-04-27 12:42:45 -0500

    refactor(App.vue): Removed unnecesary import

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 
-import ArtistProfilePage from '.././src/content/pages/artist-profile-page.vue'
 import BookPublishPage2 from '@/content/pages/book-publish-page-2.vue'
 </script>
 
 <template>
-<!--  <artist-profile-page/>-->
   <book-publish-page2/>
 </template>
 
