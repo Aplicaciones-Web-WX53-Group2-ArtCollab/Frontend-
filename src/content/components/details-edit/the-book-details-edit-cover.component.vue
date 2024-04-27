a3df39150e1b26e68b844adae09dd74f1aba9839
CamiAm404 <u202114248@upc.edu.pe>
2024-04-26 21:03:28 -0500

    feat(details-edit-cover)/chore(assets): Added component and image
    Added component and book cover image.

---
+++ src/content/components/details-edit/the-book-details-edit-cover.component.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+
+const fileInput = ref(null);
+
+const onIconClick = () => {
+  fileInput.value.click();
+};
+
+const onFileChange = (event) => {
+  const file = event.target.files[0];
+
+  // Verificar si el archivo es una imagen
+  if (!file.type.startsWith('image/')) {
+    console.error('El archivo seleccionado no es una imagen');
+    return;
+  }
+
+  // Aquí puedes manejar la subida del archivo
+  console.log(file);
+};
+</script>
+
+<template>
+  <div class="edit-cover relative w-11rem h-12rem mx-3 my-3 md:my-0 border-round">
+    <img src="../../../assets/images/Boulevard-Cover.jpg" class="w-10 h-auto relative">
+    <div class="pencil-icon absolute border-circle w-2rem h-2rem bg-cyan-500 text-white-alpha-90 flex align-items-center justify-content-center bottom-0 left-0">
+      <i class="pi pi-pencil" @click="onIconClick"/>
+    </div>
+    <input type="file" ref="fileInput" @change="onFileChange" accept="image/*" style="display: none" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
