506b0a7d7d645ae8620ab1f378cef62427625f86
CamiAm404 <u202114248@upc.edu.pe>
2024-04-26 22:58:44 -0500

    feat(details-edit-tabview): Change defaultValue2

--- src/content/components/details-edit/the-book-details-edit-tabview.component.vue
+++ src/content/components/details-edit/the-book-details-edit-tabview.component.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue';
 
 const defaultValue = ref('Boulevard');
-const defaultValue2 = ref('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+const defaultValue2 = ref('¿Quién dijo que después de la tormenta sale el sol cuando puede haber un rayo? Vuelve el fenómeno literario de mayor éxito de los últimos tiempos en una edición revisada por la autora. Luke Howland, lleno de problemas y sumido en una desesperación profunda, y Hasley Weigel, tan despistada como optimista, no se ajustan al prototipo de pareja perfecta. Como si cada uno fuese un cielo uno es tormenta y el otro un día soleado: él es oscuridad. Ella un rayo de sol. Y, sin embargo, juntos deciden ponerle nombre a lo que habían creado: un boulevard teñido de tonos grisáceos, celestes y azules eléctricos preparándose para la tormenta. Ella era para él y él era para ella. Una historia de amor tan única que te marcará para el resto de tus días.');
 </script>
 
 <template>
@@ -11,15 +11,15 @@ const defaultValue2 = ref('Lorem ipsum dolor sit amet, consectetur adipiscing el
       <div class="flex flex-column">
         <pv-tabpanel header="Detalles de la historia">
           <div class="flex align-items-center justify-content-center mb-5 mt-3">
-            <div class="flex flex-column">
+            <div class="titulo flex flex-column">
               <label class="text-2xl font-bold" for="titulo">Título</label>
               <pv-textarea v-model="defaultValue" rows="1" cols="50" />
             </div>
           </div>
           <div class="flex align-items-center justify-content-center mb-5">
-            <div class="flex flex-column">
+            <div class="descripcion flex flex-column">
               <label class="text-2xl font-bold" for="titulo">Descripción</label>
-              <pv-textarea v-model="defaultValue2" rows="9" cols="50" />
+              <pv-textarea v-model="defaultValue2" rows="14" cols="50" />
             </div>
           </div>
         </pv-tabpanel>
