1386f9605eb3ce7dd9448652701d841d5f7d025c
CamiAm404 <u202114248@upc.edu.pe>
2024-04-27 12:40:36 -0500

    feat(artist-profile-page): Added components
    Added navbar, user login and footer components.

--- src/user/pages/artist-profile-page.vue
+++ src/user/pages/artist-profile-page.vue
@@ -1,15 +1,29 @@
-<script>
+<script setup>
 import ArtistProfileComponent from '@/user/components/artist-profile.component.vue'
-export default {
-  name: 'artist-profile-page',
-  components: {
-    ArtistProfileComponent,
-  }
-}
+import TheUserLogin from '@/user/components/the-user-login.component.vue'
+import TheUserLoginEmail from '@/user/components/the-user-login-email.component.vue'
+import NavbarContent from '@/public/components/navbar-content.component.vue'
+import FooterContent from '@/public/components/footer-content.component.vue'
+import { ref } from 'vue'
+
+const showLogin = ref(false);
+const showEmailLogin = ref(false);
+
+const toggleLogin = () => {
+  showLogin.value = !showLogin.value;
+};
+
+const toggleEmailLogin = () => {
+  showEmailLogin.value = !showEmailLogin.value;
+};
 </script>
 
 <template>
+  <navbar-content :showLogin="showLogin" :toggleLogin="toggleLogin" />
+  <the-user-login :showLogin="showLogin" :toggleLogin="toggleLogin" @showEmailLogin="toggleEmailLogin"/>
+  <the-user-login-email v-if="showEmailLogin" @back="toggleEmailLogin" />
   <artist-profile-component/>
+  <footer-content/>
 </template>
 
 <style scoped>
