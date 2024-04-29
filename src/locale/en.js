7af5527898040ab9fcf76b4c587bc63e9892ccb8
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-04-29 16:30:09 -0500

    feat: Added Locale

---
+++ src/locale/en.js
@@ -0,0 +1,24 @@
+const en = {
+    gender: 'Gender',
+    popular: 'Popular',
+    subscription: 'Subscription',
+    monetization: 'Monetization',
+    artists: 'Artists',
+    login: 'Log in',
+    about: 'About',
+    copyright: 'All rights reserved',
+    artist: 'Artist',
+    subs: 'Subscription',
+    email: 'Email',
+    password: 'Password',
+    name: 'Name',
+    account: 'Account',
+    textaccount: 'Change your account information and privacy settings',
+    change: 'Change',
+    delete: 'Cancel',
+    submit: 'Submit',
+    want: 'Or do you want',
+    changeprofile: 'Change your profile'
+}
+
+export default en
---
+++ src/locale/es.js
@@ -0,0 +1,24 @@
+const es = {
+    gender: 'Género',
+    popular: 'Populares',
+    subscription: 'Suscripciones',
+    monetization: 'Monetización',
+    artists: 'Artistas',
+    login: 'Iniciar sesión',
+    about: 'Acerca de',
+    copyright: 'Todos los derechos reservados',
+    artist: 'Artista',
+    subs: 'Suscripcion' ,
+    email: 'Correo electronico',
+    password: 'Contraseña',
+    name: 'Nombre',
+    account: 'Cuenta',
+    textaccount: 'Cambia la informacion de tu cuenta y la configuracion de privacidad',
+    change: 'Cambiar',
+    delete: 'Cancelar',
+    submit: 'Enviar',
+    want: 'O deseas',
+    changeprofile: 'Cambiar tu perfil',
+}
+
+export default es
