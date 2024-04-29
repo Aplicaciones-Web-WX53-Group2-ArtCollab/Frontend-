350ba65287803166d6a8a4e060a40e4c5a879ddc
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-04-29 17:43:25 -0500

    feat(locale): Add Incomes file in Locale

---
+++ src/locale/Incomes/incomes_en.js
@@ -0,0 +1,10 @@
+const incomes_en = {
+    details: 'Edit story details',
+    cancel: 'Cancel',
+    save: 'Save',
+    principal: 'Principal',
+    recent: 'Recent',
+    income: 'Income obtained',
+}
+
+export default incomes_en
