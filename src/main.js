2e1d44f7c4f730eef38cedf989f8fcd5ee314640
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-04-29 16:31:11 -0500

    feat(Main.js):Added new Components

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import PrimeVue from "primevue/config";
+import Card from "primevue/card";
+import Button from "primevue/button";
+import Toolbar from 'primevue/toolbar';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
+import Sidebar from 'primevue/sidebar';
+import i18n from '@/locale/i18n.js'
+
+
+import 'primeicons/primeicons.css'
+import 'primevue/resources/primevue.min.css'
+
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('pv-card', Card);
+app.component('pv-toolbar', Toolbar);
+app.component('pv-iconfield', IconField);
+app.component('pv-inputicon', InputIcon);
+app.component('pv-inputtext', InputText);
+app.component('pv-sidebar', Sidebar);
+app.component('pv-button', Button);
+
+
+app.use(i18n);
+
+
+app.mount('#app')
