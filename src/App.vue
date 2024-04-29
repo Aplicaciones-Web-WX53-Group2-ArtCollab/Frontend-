edd4226be8cc1c3870e17cfa63114392314d759e
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-04-29 16:32:42 -0500

    feat(Components): Added new Component

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import CuentaInfo from "@/components/cuenta-info.component.vue";
+import NavbarContent from "@/public/components/navbar-content.component.vue";
+import FooterContent from "@/public/components/footer-content.component.vue";
+</script>
+
+<template>
+  <navbar-content></navbar-content>
+  <cuenta-info></cuenta-info>
+  <footer-content></footer-content>
+</template>
+
+<style scoped>
+
+</style>
