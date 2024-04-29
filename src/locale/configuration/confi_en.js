e0ce21d407135812b0db6f4c8faa76ea9285765f
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-04-29 16:47:16 -0500

    feat(locale): Added configuration file

---
+++ src/locale/configuration/confi_en.js
@@ -0,0 +1,16 @@
+const sdddd = {
+    artist: 'Artist',
+    subs: 'Subscription',
+    email: 'Email',
+    password: 'Password',
+    name: 'Name',
+    account: 'Account',
+    textaccount: 'Change your account information and privacy settings',
+    change: 'Change',
+    delete: 'Cancel',
+    submit: 'Submit',
+    want: 'Or do you want',
+    changeprofile: 'Change your profile'
+}
+
+export default sdddd
---
+++ src/locale/configuration/confi_es.js
@@ -0,0 +1,16 @@
+const ddffdf = {
+    artist: 'Artista',
+    subs: 'Suscripcion' ,
+    email: 'Correo electronico',
+    password: 'Contraseña',
+    name: 'Nombre',
+    account: 'Cuenta',
+    textaccount: 'Cambia la informacion de tu cuenta y la configuracion de privacidad',
+    change: 'Cambiar',
+    delete: 'Cancelar',
+    submit: 'Enviar',
+    want: 'O deseas',
+    changeprofile: 'Cambiar tu perfil',
+}
+
+export default ddffdf
