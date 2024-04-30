42f933b4896141fb4bd66d321e6ee1fe8cdd1b13
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-04-30 14:33:31 -0500

    feat(locale): Add Chapter file in Locale

---
+++ src/locale/chapter/chapter_en.js
@@ -0,0 +1,16 @@
+const en = {
+    chapter: "Chapter 2",
+    caps: "You have 2 more chapters for free",
+    share: "Show your support to the authors of this series by sharing it!",
+    like: 'Like',
+    follow: "Follow",
+    author: 'Author',
+    artist: 'Artist',
+    coments: "Comments",
+    logincoment: "Login to leave your comment...",
+    principal: "Principal",
+    recent: "Recent",
+    nocoment: "There are no comments recently."
+}
+
+export default en
