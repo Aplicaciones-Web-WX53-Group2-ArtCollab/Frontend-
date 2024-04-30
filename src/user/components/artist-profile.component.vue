7929e7cd8b9ca8a1fa5e37892b3a7de791315965
CamiAm404 <u202114248@upc.edu.pe>
2024-04-30 18:42:08 -0500

    feat(artist-profile): Added API call

--- src/user/components/artist-profile.component.vue
+++ src/user/components/artist-profile.component.vue
@@ -1,14 +1,36 @@
 <script>
+import { Portfolio } from '@/user/models/portfolio.entity.js'
+import { PotfolioEndpoint } from '@/user/services/portfolio-endpoint.service.js'
 export default {
   name: 'the-artist-profile',
   data() {
     return {
       social_media: 'Social Media',
       illustrated_books: 'Illustrated Books',
-      portfolio: 'Portfolio'
-    };
+      portfolioImage1: '',
+      portfolioImage2: '',
+      portfolioImage3: '',
+      portfolios: [],
+      portfolio: new Portfolio(),
+      portfolioApiFake: new PotfolioEndpoint()
+    }
+  },
+  async created() {
+    this.portfolioApiFake.getAllPortfolios().then((response) => {
+      response.data.forEach((portfolioData) => {
+        const { id, reader_id, create_at, title, description, quantity, images } = portfolioData;
+        if (portfolioData.id === 3) {
+          this.portfolio = new Portfolio(id, reader_id, create_at, title, description, quantity, images);
+          this.portfolioImage1 = images.image_1;
+          this.portfolioImage2 = images.image_2;
+          this.portfolioImage3 = images.image_3;
+        }
+      });
+    }).catch((error) => {
+      console.error('Error fetching portfolios:', error);
+    });
   }
-};
+}
 
 </script>
 
@@ -17,7 +39,9 @@ export default {
     <div class="banner">
       <div class="banner-background"></div>
       <div class="profile-image">
-        <img src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/artist-profile-image.png?raw=true" width="496" alt="Artist profile image">
+        <img
+          src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/artist-profile-image.png?raw=true"
+          width="496" alt="Artist profile image">
         <div class="text">Miriam Bonastre</div>
       </div>
     </div>
@@ -45,11 +69,15 @@ export default {
         <div class="artist-books">
           <div class="artist-books-title">{{ $t('artistProfile.illustrated_books') }}</div>
           <div class="flex-container-2">
-            <img class="normal-image" src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/marionetta-cover.png?raw=true" alt="Book 1 image">
+            <img class="normal-image"
+                 src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/marionetta-cover.png?raw=true"
+                 alt="Book 1 image">
             <p>Marionetta</p>
           </div>
           <div class="flex-container-2">
-            <img class="normal-image" src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/hooky-cover.png?raw=true" alt="Book 2 image">
+            <img class="normal-image"
+                 src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/hooky-cover.png?raw=true"
+                 alt="Book 2 image">
             <p>Hooky</p>
           </div>
         </div>
@@ -58,15 +86,18 @@ export default {
         <div class="bio">
           <div class="bio-title">Bio</div>
           <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit mauris ornare malesuada himenaeos eleifend morbi, vivamus nostra faucibus platea nisl nec lacinia hendrerit ultricies id maecenas diam. Eget vel aliquet nam penatibus vulputate felis dapibus magnis montes auctor ut, augue placerat praesent class condimentum litora eros sociis iaculis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit mauris ornare malesuada himenaeos eleifend morbi,
+              vivamus nostra faucibus platea nisl nec lacinia hendrerit ultricies id maecenas diam. Eget vel aliquet nam
+              penatibus vulputate felis dapibus magnis montes auctor ut, augue placerat praesent class condimentum
+              litora eros sociis iaculis.</p>
           </div>
         </div>
         <div class="portfolio">
           <div class="portfolio-title">{{ $t('artistProfile.portfolio') }}</div>
           <div class="flex-container">
-            <img class="normal-image" src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/portfolio-1.png?raw=true" alt="Portfolio first image">
-            <img class="normal-image" src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/portfolio-2.png?raw=true" alt="Portfolio first image">
-            <img class="large-image" src="https://github.com/Aplicaciones-Web-WX53-Group2-ArtCollab/Frontend-/blob/bounded-context/content/src/assets/images/portfolio-3.png?raw=true" alt="Portfolio first image">
+            <img class="normal-image" :src="portfolioImage1" alt="Portfolio first image">
+            <img class="normal-image" :src="portfolioImage2" alt="Portfolio first image">
+            <img class="large-image" :src="portfolioImage3" alt="Portfolio first image">
           </div>
         </div>
       </div>
@@ -100,7 +131,7 @@ export default {
   color: black;
   font: bold 30px/30px Archivo, sans-serif;
   z-index: 2; /* z-index mayor para que el texto esté delante de la imagen */
-  top:-10%;
+  top: -10%;
 }
 
 .flexbox-container {
@@ -118,12 +149,14 @@ export default {
   font: 20px/30px Archivo, sans-serif;
   color: black;
 }
+
 .profile-image {
   position: relative; /* Asegúrate de que .elemento tenga una posición que no sea 'static' para que z-index funcione */
-  text-align:center;
+  text-align: center;
   padding: 20px;
   z-index: 3; /* z-index aún mayor para que esté delante de ambos fondos */
 }
+
 .flex-container-2 {
   display: flex;
   justify-content: flex-start;
@@ -189,71 +222,85 @@ export default {
   justify-content: center;
 }
 
-.flex-container-2 a{
+.flex-container-2 a {
   display: flex;
   justify-content: center;
   color: black;
   text-decoration: underline;
 }
 
-@media (width<=540px) {
-  .profile-image img{
+@media (width <= 540px) {
+  .profile-image img {
     width: 350px;
   }
-  .social-media p{
+
+  .social-media p {
     font-size: 10px;
   }
-  .bio p{
+
+  .bio p {
     font-size: 10px;
   }
-  .banner-background{
+
+  .banner-background {
     height: 35vh;
-  }
-}
-@media (width<=430px) {
-  .profile-image img{
-    width: 330px;
-  }
-  .social-media p{
-    font-size: 8px;
-  }
-  .bio p{
-    font-size: 10px;
-  }
-  .banner-background{
-    height: 35vh;
-    width:450px;
   }
 }
 
-@media (width<=375px) {
-  .profile-image img{
-    width: 320px;
+@media (width <= 430px) {
+  .profile-image img {
+    width: 330px;
   }
-  .social-media p{
+
+  .social-media p {
     font-size: 8px;
   }
-  .bio p{
+
+  .bio p {
     font-size: 10px;
   }
-  .banner-background{
-    height: 45vh;
-    width:400px;
+
+  .banner-background {
+    height: 35vh;
+    width: 450px;
   }
 }
-@media (width<=344px) {
-  .profile-image img{
-    width: 300px;
+
+@media (width <= 375px) {
+  .profile-image img {
+    width: 320px;
   }
-  .social-media p{
+
+  .social-media p {
     font-size: 8px;
   }
-  .bio p{
+
+  .bio p {
     font-size: 10px;
   }
-  .banner-background{
+
+  .banner-background {
+    height: 45vh;
+    width: 400px;
+  }
+}
+
+@media (width <= 344px) {
+  .profile-image img {
+    width: 300px;
+  }
+
+  .social-media p {
+    font-size: 8px;
+  }
+
+  .bio p {
+    font-size: 10px;
+  }
+
+  .banner-background {
     height: 30vh;
-    width:350px;
+    width: 350px;
   }
 }
 </style>
