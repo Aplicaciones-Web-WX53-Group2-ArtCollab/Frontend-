2e41fff3bf20b12b9d91a1843cef28af3f480109
CamiAm404 <u202114248@upc.edu.pe>
2024-04-30 13:52:17 -0500

    feat(book-publish-page-1): Added i18n variables to page and its components

--- src/content/components/publish/book-publish-banner.component.vue
+++ src/content/components/publish/book-publish-banner.component.vue
@@ -7,12 +7,12 @@
     <div class="banner flex flex-row justify-content-center align-content-center text-white">
       <div class="banner-1 flex flex-row justify-content-center align-items-center">
         <p class="bg-cyan-600">1</p>
-        <div>Libros</div>
+        <div>{{ $t('bookPublish.book') }}</div>
       </div>
       <div class="banner-2 flex flex-row justify-content-center align-items-center">
         <i class="pi pi-arrow-right"></i>
         <p class="bg-cyan-600">2</p>
-        <div>Capitulos</div>
+        <div>{{ $t('bookPublish.chapters') }}</div>
       </div>
     </div>
   </div>
