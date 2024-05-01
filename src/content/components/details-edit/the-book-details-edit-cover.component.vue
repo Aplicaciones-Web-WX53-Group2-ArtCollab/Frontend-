a041e25ce458b1b23dc1caa63b884a308dd814fb
CamiAm404 <u202114248@upc.edu.pe>
2024-04-30 19:11:41 -0500

    feat(details-edit-cover): Unnecessary information removed

--- src/content/components/details-edit/the-book-details-edit-cover.component.vue
+++ src/content/components/details-edit/the-book-details-edit-cover.component.vue
@@ -4,10 +4,9 @@ import { BookInternalService } from '@/content/services/book-internal.service.js
 
 
 export default {
-  name: 'all-books-cards',
+  name: 'book-details-edit-cover',
   data(){
     return{
-      books: [],
       book: new Book(),
       bookPopular: new Book(),
       bookApiFake:new BookInternalService()
@@ -19,13 +18,8 @@ export default {
         const { title, description, datePublish, type, id, imgUrl, likes, views } = bookData;
         if (bookData.type === 'book' && id === '1') {
           this.book = new Book(title, description, datePublish, type, id, imgUrl, likes, views);
-          this.books.push(this.book);
-          if (bookData.views > this.bookPopular.views) {
-            this.bookPopular = bookData;
-          }
         }
       });
-      this.books = this.books.filter(book => book.id !== this.bookPopular.id);
     }).catch((error) => {
       console.error('Error fetching books:', error);
     });
@@ -59,7 +53,7 @@ const onFileChange = (event) => {
 
 <template>
   <div class="edit-cover relative w-19rem h-21rem mx-3 my-3 md:my-0 border-round">
-    <img :src="bookPopular.imgUrl" class="w-10 h-auto relative">
+    <img :src="book.imgUrl" class="w-10 h-auto relative">
     <div
       class="pencil-icon absolute border-circle w-2rem h-2rem bg-cyan-500 text-white-alpha-90 flex align-items-center justify-content-center bottom-0 left-0 ml-3">
       <i class="pi pi-pencil" @click="onIconClick" />
