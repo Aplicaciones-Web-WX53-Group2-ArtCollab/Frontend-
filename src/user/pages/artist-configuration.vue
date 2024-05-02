c51e83dfe008b1b822f904d972e800a108443158
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-05-02 10:49:13 -0500

    feat: artist-configuration page

---
+++ src/user/pages/artist-configuration.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import navbarContentComponent from '@/public/components/navbar-content.component.vue'
+  import footerContentComponent from '@/public/components/footer-content.component.vue'
+  import theUserConfigurationComponent from '../components/the-user-configuration.component.vue';
+  export default {
+    name: 'artistConfigurationPage',
+  }
+</script>
+
+<template>
+  <navbarContentComponent/>
+  <theUserConfigurationComponent/>
+  <footerContentComponent/>
+
+</template>
+
+<style scoped>
+
+</style>
