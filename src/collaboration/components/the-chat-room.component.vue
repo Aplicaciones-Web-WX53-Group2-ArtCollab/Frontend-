7f9c00969751faed96e1cd7e91ec658317d6f025
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-05-02 10:34:20 -0500

    fix: the-chat-room in components

--- src/collaboration/components/the-chat-room.component.vue
+++ src/collaboration/components/the-chat-room.component.vue
@@ -1,37 +1,78 @@
 <script >
+
 export default {
-  name: 'the-chat-room',
+  name: 'chat-artistas',
+  data() {
+  return {
+    messages: [
+      { sender: 'User 1', text: 'Lorem ipsum dolor sit amet' },
+      { sender: 'You', text: 'Consectetur adipiscing elit' },
+      { sender: 'User 1', text: 'Sed do eiusmod tempor incididunt' },
+      { sender: 'You', text: 'Ut labore et dolore magna aliqua' }
+    ],
+    newMessage: ''
+  }
+},
+methods: {
+  sendMessage() {
+    if (this.newMessage.trim() !== '') {
+      this.messages.push({ sender: 'You', text: this.newMessage });
+      this.newMessage = '';
+    }
+  }
+}
 }
 </script>
 
 <template>
 
   <pv-toolbar class="flex border-noround text-white-alpha-90 bg-white">
-    <template #start>
-      <div class="flex justify-content-start" aria-label="Navigation bar">
+    <template #start class="flex justify-content-start">
+      <div class="flex justify-content-start">
         <pv-button icon="pi pi-chevron-left" class="text-black-alpha-90 border-none bg-transparent"></pv-button>
         <div class="text-black-alpha-90">
-          <p class="text-xl">{{$t('rooms')}}</p>
+          <p class="text-xl">{{$t('room')}}</p>
         </div>
       </div>
     </template>
     <hr/>
   </pv-toolbar>
-  <div class="bg-white flex justify-content-between" aria-label="Main content area">
-    <div aria-label="Chat area"><h1>Aqui va el chat</h1></div>
-  <div class="cyan border-round-2xl" aria-label="User profile area">
+  <div class="bg-white flex justify-content-between">
+    <div class="text-black-alpha-90 width">
+      <div>
+        <h1 class="text-black-alpha-90 text-2xl ml-6">Chat</h1>
+        <div class=" flex justify-content-between">
+          <div class="flex">
+          <img class="ml-6" src="/src/assets/miriam.png" alt="Artistas" width="40" height="48" />
+          <p class="text-lg">Miriam Bonastre</p>
+        </div>
+          <div class="ml-8">
+            <i class="pi pi-phone"></i>
+            <i class="pi pi-camera"></i>
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <div class="ml-8 chat-container">
+        <div v-for="(message, index) in messages" :key="index" :class="{'message-user1': message.sender === 'User 1', 'message-you': message.sender === 'You'}">
+          <p><strong>{{ message.sender }}:</strong> {{ message.text }}</p>
+        </div>
+        <input type="text" v-model="newMessage" @keyup.enter="sendMessage" class="input-message">
+      </div>
+    </div>
+  <div class="cyan border-round-2xl">
     <div class="column-gap-0">
       <img class="ml-6" src="/src/assets/miriam.png" alt="Artistas" width="186" height="189" />
       <h1 class="text-black-alpha-90 ml-4">Miriam Bonastre</h1>
-      <div class="flex mt-4" aria-label="User actions">
+      <div class="flex mt-4">
         <i class="pi pi-user mr-2 text-black-alpha-90 text-xl ml-8"></i>
         <i class="pi pi-search text-black-alpha-90 text-xl ml-6"></i>
       </div>
-      <div class="flex" aria-label="Navigation links">
+      <div class="flex">
         <p class="text-black-alpha-90 text-lg ml-7">{{$t('profile')}}</p>
         <p class="text-black-alpha-90 text-lg ml-6">{{$t('search')}}</p>
       </div>
-      <div class="flex mt-4" aria-label="Settings">
+      <div class="flex mt-4">
         <p class="text-black-alpha-90 text-lg">{{$t('settings')}}</p>
         <pv-button icon="pi pi-chevron-right" class="text-black-alpha-90 border-none bg-transparent"></pv-button>
 
@@ -43,5 +84,28 @@ export default {
 <style >
 .cyan{
   background-color: #11AEBF;
+}
+.width {
+  width: 100%;
+}
+.chat-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.message-user1 {
+  align-self: flex-start;
+}
+
+.message-you {
+  align-self: flex-end;
+}
+
+.input-message {
+  width: 100%;
+  position: absolute;
+  bottom: 120px;
 }
 </style>
