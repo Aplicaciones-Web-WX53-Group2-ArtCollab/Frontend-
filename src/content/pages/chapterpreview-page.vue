3a7baa95a7bf4818414b122f8a6def7c744b99aa
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-05-02 10:58:38 -0500

    feat: chapters in pages

---
+++ src/content/pages/chapterpreview-page.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ThePreviewchapter from '@/content/components/the-previewchapter.component.vue'
+import NavbarContent from '@/public/components/navbar-content.component.vue'
+import FooterContent from '@/public/components/footer-content.component.vue'
+</script>
+
+<template>
+  <navbar-content/>
+    <the-previewchapter/>
+  <footer-content/>
+</template>
+
+<style scoped>
+
+</style>
