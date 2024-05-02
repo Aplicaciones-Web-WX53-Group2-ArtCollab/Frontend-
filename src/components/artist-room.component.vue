a2e92639a43a1bb1a27b574df85acc1062206aea
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-05-01 22:52:50 -0500

    fix: Aria Labels in artist-room

--- src/components/artist-room.component.vue
+++ src/components/artist-room.component.vue
@@ -5,19 +5,19 @@ export default {
 </script>
 
 <template>
-  <div class="bg-white">
+  <div class="bg-white" aria-label="Main content area">
     <pv-card class="bg-yellow-300	border-noround mb-6">
   <template #content>
-    <div class="flex justify-content-between" aria-label="Edit content between">
-    <div class="align-content-center" aria-label="">
+    <div class="flex justify-content-between" aria-label="Room header">
+    <div class="align-content-center" aria-label="Room title">
       <h1 class="text-4xl font-bold">{{ $t('room') }}</h1>
     </div>
-    <div class="sm:flex">
-      <div class="column">
+    <div class="sm:flex" aria-label="Artists profiles">
+      <div class="column" aria-label="Miriam profile">
         <img class="sm:w-auto" src="/src/assets/miriam.png" alt="Miriam" width="223" height="224"/>
         <p class="text-xl"> Miriam Bonastre</p>
       </div>
-      <div class="column">
+      <div class="column" aria-label="Flor profile">
         <img class="sm:w-auto" src="/src/assets/flor.png" alt="Flor" width="223" height="224"/>
         <p class="text-xl">Flor M. Salvador</p>
       </div>
@@ -27,28 +27,28 @@ export default {
 </pv-card>
   <pv-card class="border-noround surface-100 mb-5">
     <template #content>
-      <div class=" flex justify-content-end">
+      <div class=" flex justify-content-end" aria-label="Chat creation">
         <p>{{ $t('chat') }} <i class="pi pi-plus"></i></p>
       </div>
     </template>
   </pv-card>
   <pv-card class="border-noround surface-100 mb-8">
     <template #content>
-      <div class=" flex justify-content-start">
+      <div class=" flex justify-content-start" aria-label="New chat message">
           <img class="sm:w-auto" src="/src/assets/miriam.png" alt="Miriam" width="70" height="70"/>
          <div class="column">
            <p class="text-bold">{{ $t('newchat') }} Miriam</p>
            <p>{{ $t('text') }}</p>
          </div>
       </div>
-      <div class=" flex justify-content-end text-center">
+      <div class=" flex justify-content-end text-center" aria-label="Message time">
         <p>6:19pm</p>
       </div>
     </template>
   </pv-card>
   <pv-card class="border-noround text-red-500	">
     <template #content>
-    <p>{{ $t('deleteroom') }}</p>
+    <p aria-label="Delete room option">{{ $t('deleteroom') }}</p>
     </template>
   </pv-card>
   </div>
