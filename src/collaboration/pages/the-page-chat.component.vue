c3a029bf173d3b19c1da32301f13e519b96a9206
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-05-02 10:06:53 -0500

    feat: Add server

--- src/collaboration/pages/the-page-chat.component.vue
+++ src/collaboration/pages/the-page-chat.component.vue
@@ -1,15 +1,17 @@
 <script>
 import NavbarContent from '@/public/components/navbar-content.component.vue'
 import FooterContent from '@/public/components/footer-content.component.vue'
+import {ChatRoom} from '@/collaboration/components/the-chat-room.component.vue'
 
 export default {
   name: 'the-page-chat',
-  components: {  FooterContent, NavbarContent }
+  components: {  FooterContent, NavbarContent, ChatRoom }
 }
 </script>
 
 <template>
   <navbar-content/>
+  <chat-room/>
   <footer-content/>
 </template>
 
