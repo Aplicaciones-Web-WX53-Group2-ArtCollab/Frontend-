6592af48e77d53dcdcf1dc1dce773f6b64f93c52
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-05-01 22:49:38 -0500

    feat: Aria Labels in artist-room

--- src/components/artist-room.component.vue
+++ src/components/artist-room.component.vue
@@ -1,59 +1,47 @@
-<script>
+<script >
 export default {
-  name: 'artist-room'
+  name: 'artist-room',
 }
 </script>
 
 <template>
-  <div class="bg-white">
-    <pv-card class="bg-yellow-300	border-noround mb-6">
-  <template #content>
-    <div class="flex justify-content-between">
-    <div class="align-content-center">
-      <h1 class="text-4xl font-bold">{{ $t('room') }}</h1>
-    </div>
-    <div class="sm:flex">
-      <div class="column">
-        <img class="sm:w-auto" src="/src/assets/miriam.png" alt="Miriam" width="223" height="224"/>
-        <p class="text-xl"> Miriam Bonastre</p>
-      </div>
-      <div class="column">
-        <img class="sm:w-auto" src="/src/assets/flor.png" alt="Flor" width="223" height="224"/>
-        <p class="text-xl">Flor M. Salvador</p>
-      </div>
-    </div>
-    </div>
-  </template>
-</pv-card>
-  <pv-card class="border-noround surface-100 mb-5">
-    <template #content>
-      <div class=" flex justify-content-end">
-        <p>{{ $t('chat') }} <i class="pi pi-plus"></i></p>
+
+  <pv-toolbar class="flex border-noround text-white-alpha-90 bg-white">
+    <template #start class="flex justify-content-start">
+      <div class="flex justify-content-start" aria-label="Navigation bar">
+        <pv-button icon="pi pi-chevron-left" class="text-black-alpha-90 border-none bg-transparent"></pv-button>
+        <div class="text-black-alpha-90">
+          <p class="text-xl">{{$t('room')}}</p>
+        </div>
       </div>
     </template>
-  </pv-card>
-  <pv-card class="border-noround surface-100 mb-8">
-    <template #content>
-      <div class=" flex justify-content-start">
-          <img class="sm:w-auto" src="/src/assets/miriam.png" alt="Miriam" width="70" height="70"/>
-         <div class="column">
-           <p class="text-bold">{{ $t('newchat') }} Miriam</p>
-           <p>{{ $t('text') }}</p>
-         </div>
+    <hr/>
+  </pv-toolbar>
+  <div class="bg-white flex justify-content-between" aria-label="Main content area">
+    <div aria-label="Chat area"><h1>Aqui va el chat</h1></div>
+  <div class="cyan border-round-2xl" aria-label="User profile area">
+    <div class="column-gap-0">
+      <img class="ml-6" src="/src/assets/miriam.png" alt="Artistas" width="186" height="189" />
+      <h1 class="text-black-alpha-90 ml-4">Miriam Bonastre</h1>
+      <div class="flex mt-4" aria-label="User actions">
+        <i class="pi pi-user mr-2 text-black-alpha-90 text-xl ml-8"></i>
+        <i class="pi pi-search text-black-alpha-90 text-xl ml-6"></i>
       </div>
-      <div class=" flex justify-content-end text-center">
-        <p>6:19pm</p>
+      <div class="flex" aria-label="Navigation links">
+        <p class="text-black-alpha-90 text-lg ml-7">{{$t('profile')}}</p>
+        <p class="text-black-alpha-90 text-lg ml-6">{{$t('search')}}</p>
       </div>
-    </template>
-  </pv-card>
-  <pv-card class="border-noround text-red-500	">
-    <template #content>
-    <p>{{ $t('deleteroom') }}</p>
-    </template>
-  </pv-card>
+      <div class="flex mt-4" aria-label="Settings">
+        <p class="text-black-alpha-90 text-lg">{{$t('settings')}}</p>
+        <pv-button icon="pi pi-chevron-right" class="text-black-alpha-90 border-none bg-transparent"></pv-button>
+
+      </div>
+    </div>
+    </div>
   </div>
 </template>
-
-<style scoped>
-
+<style >
+.cyan{
+  background-color: #11AEBF;
+}
 </style>
