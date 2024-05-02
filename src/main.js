9a7bd39ab885a41167873d4a84f2f558873d0625
Gustavo Huilca <u202213983@upc.edu.pe>
2024-05-02 17:25:22 -0500

    fix: Corrected files from merge

--- src/main.js
+++ src/main.js
@@ -6,23 +6,11 @@ import 'primevue/resources/primevue.min.css'
 import 'primevue/resources/themes/aura-light-green/theme.css'
 
 import { createApp } from 'vue'
-
-import App from './App.vue';
-
-import 'primevue/resources/themes/aura-light-green/theme.css';
-import "primeflex/primeflex.css";
-import 'primeicons/primeicons.css';
-import PrimeVue from 'primevue/config'
 import router from '@/router.js'
 
-import Toolbar from 'primevue/toolbar';
-import Button from 'primevue/button';
 import Divider from 'primevue/divider';
-import Sidebar from 'primevue/sidebar';
-import Card from 'primevue/card';
 import Password from 'primevue/password';
 import Checkbox from 'primevue/checkbox'
-import Image from 'primevue/image'
 import DataView from 'primevue/dataview';
 
 import ToastService from "primevue/toastservice";
@@ -33,15 +21,11 @@ import ProgressBar from "primevue/progressbar";
 import Badge from "primevue/badge";
 import Dropdown from "primevue/dropdown";
 import Galleria from 'primevue/galleria'
-import i18n from '@/locale/i18n.js'
 import Rating from 'primevue/rating'
-import InputText from "primevue/inputtext";
 import Editor from 'primevue/editor';
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
 import Textarea from 'primevue/textarea'
-import InputIcon from 'primevue/inputicon'
-import IconField from 'primevue/iconfield'
 import App from './App.vue'
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import PrimeVue from "primevue/config";
@@ -63,9 +47,6 @@ import 'primevue/resources/primevue.min.css'
 
 const app = createApp(App);
 
-app.use(PrimeVue);
-
-const app = createApp(App);
 app.use(PrimeVue);
 app.component('pv-button', Button);
 app.component('pv-toolbar', Toolbar);
@@ -89,8 +70,8 @@ app.component('pv-textarea', Textarea);
 app.component('pv-sidebar', Sidebar);
 app.component('pv-password', Password);
 app.component('pv-checkbox', Checkbox);
-app.component('pv-image', Image);
 app.component('pv-dataView', DataView);
+app.component('pv-image', Image);
 app.use(router);
 app.use(i18n);
 app.use(ToastService);
@@ -104,18 +85,6 @@ app.use(VueMq, {
   defaultBreakpoint: 'sm',
 });
 
-app.mount('#app')
-app.component('pv-card', Card);
-app.component('pv-toolbar', Toolbar);
-app.component('pv-iconfield', IconField);
-app.component('pv-inputicon', InputIcon);
-app.component('pv-inputtext', InputText);
-app.component('pv-sidebar', Sidebar);
-app.component('pv-button', Button);
-app.component('pv-image', Image);
-
-
-app.use(i18n);
 
 
 app.mount('#app')
