697dca4d445dfe382c5b979978ed7c4119271e45
CamiAm404 <u202114248@upc.edu.pe>
2024-05-02 21:52:08 -0500

    feat(develop): Added router-links to components

--- src/content/components/publish/book-publish-banner.component.vue
+++ src/content/components/publish/book-publish-banner.component.vue
@@ -6,12 +6,16 @@
   <div class="container3 flex justify-content-center align-items-center bg-orange-300 mt-3 mb-5" aria-label="Banner">
     <div class="banner flex flex-row justify-content-center align-content-center text-white">
       <div class="banner-1 flex flex-row justify-content-center align-items-center" aria-label="Step 1">
-        <p class="bg-cyan-600">1</p>
+        <router-link to="/publish-1">
+          <p class="bg-cyan-600">1</p>
+        </router-link>
         <div>{{ $t('book') }}</div>
       </div>
       <div class="banner-2 flex flex-row justify-content-center align-items-center" aria-label="Step 2">
         <i class="pi pi-arrow-right"></i>
-        <p class="bg-cyan-600">2</p>
+        <router-link to="/publish-2">
+          <p class="bg-cyan-600">2</p>
+        </router-link>
         <div>{{ $t('chapters') }}</div>
       </div>
     </div>
@@ -31,6 +35,21 @@
 
 .banner-1 {
   margin-right: 8rem;
+}
+
+.router-link-exact-active,
+.router-link-active {
+  color: inherit;
+}
+
+.router-link-exact-active:hover,
+.router-link-active:hover {
+  background-color: transparent;
+}
+
+.router-link-exact-active p:hover,
+.router-link-active p:hover {
+  background-color: transparent;
 }
 
 @media (max-width: 576px) {
