f1ae063a1ecc83a67a507c8f8f11588f979f85e3
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-06-26 10:45:53 -0500

    feat: set token to localstorage

--- src/content/services/book-internal.service.js
+++ src/content/services/book-internal.service.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const http = axios.create({
     baseURL: 'https://art-collab.azurewebsites.net/api/v1/content',
+    localStorage: localStorage.setItem('token','eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTk5NTI5MDQsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZ3VzdGF2bzEyIiwiaWF0IjoxNzE5MzQ4MTA0LCJuYmYiOjE3MTkzNDgxMDR9.7m-JOueWg38U0L2z8m84yrWruKQ8Ew-kyoGtefwIyYs'),
     headers: {
         'Authorization': 'Bearer ' + localStorage.getItem('token')
     }
